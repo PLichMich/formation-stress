577e5c5c648aed010ef596fb1f4f02426dc8e2d6
Michaël Plichart <michael.plichart@gmail.com>
2026-03-22 15:35:06 +0100

    Changement_liens_sociaux

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://www.stressaction.be', // Vérifie bien cette ligne !
+    // ... le reste de ta config
+});
