3a84a20759f85e36562cd146df89b72a749af583
Michaël Plichart <michael.plichart@gmail.com>
2026-03-24 10:04:45 +0100

    Deploiement_github

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-    site: 'https://www.stressaction.be', // Vérifie bien cette ligne !
-    // ... le reste de ta config
+  site: 'https://www.stressaction.be',
+  // On ne change rien d'autre pour l'instant
 });
