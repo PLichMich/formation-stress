ae2038b7ff45c5ca49a91b9818c521c2e6137fec
Michaël Plichart <michael.plichart@gmail.com>
2026-03-24 11:33:02 +0100

    Préparation pour le domaine perso

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://plichmich.github.io',
-  base: '/formation-stress', // Très important pour GitHub Pages !
+  site: 'https://www.stressaction.be',
 });
