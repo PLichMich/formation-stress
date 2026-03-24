b715e14d790c0aa85362b4b445a56119efe5267b
Michaël Plichart <michael.plichart@gmail.com>
2026-03-24 11:22:07 +0100

    Configuration finale de chez finale pour GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://www.stressaction.be',
-  // On ne change rien d'autre pour l'instant
+  site: 'https://plichmich.github.io',
+  base: '/formation-stress', // Très important pour GitHub Pages !
 });
